87d78446d49025580488742663db2fa7c813767d
Peptidase <ArifMeighan@hotmail.co.uk>
2018-07-06 23:04:34 +0100

    FlagFacts
    Added Flag Facts

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+function InfoOff(x)
+{
+	x.style.height = "260px";
+	x.style.width = "490px";
+	document.getElementById("CountryData").innerHTML = "Hover over a flag to see a cool fact";
+	
+}
+
+function USA(x)
+{
+	x.style.height = "520px";
+	x.style.width = "980px";
+	document.getElementById("CountryData").innerHTML = "Fact";
+
+	
+}
+
+function Scotland(x)
+{
+	x.style.height = "520px";
+	x.style.width = "980px";
+	document.getElementById("CountryData").innerHTML = "Fact";
+
+	
+}
+
+function Jamaica(x)
+{
+	x.style.height = "520px";
+	x.style.width = "980px";
+	document.getElementById("CountryData").innerHTML = "Fact";
+
+	
+}
